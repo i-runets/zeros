1819b054ec982857cb354c9d35c1ebb586c14a36
seafiv <runecivan@gmail.com>
2018-02-20 22:46:02 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var zeros = 0;
+  var iMax = Math.floor(Math.log(number) / Math.log(5));
+  for (var i = 1; i <= iMax; i++) {
+    zeros += Math.floor(number / Math.pow(5, i));
+  }
+  return zeros
 }
